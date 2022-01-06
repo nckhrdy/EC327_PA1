addc876fe84169c0fdb2140c36ebc37dac24567d
nckhrdy <91441308+nckhrdy@users.noreply.github.com>
2022-01-06 13:27:36 -0500

    Add files via upload

---
+++ Q2/PA1_Q2_Main.hpp
@@ -0,0 +1,17 @@
+//
+//  PA1_Q2_Main.hpp
+//  
+//
+//  Created by Nick Hardy on 10/7/21.
+//
+
+#ifndef PA1_Q2_Main_hpp
+#define PA1_Q2_Main_hpp
+
+
+#include <stdio.h>
+#include <iostream>
+#include <time.h>
+#include <math.h>
+
+#endif /* PA1_Q2_Main_hpp */
