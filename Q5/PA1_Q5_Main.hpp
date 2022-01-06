7aee10180670ec72df3992b7770dac5ffa26daa8
nckhrdy <91441308+nckhrdy@users.noreply.github.com>
2022-01-06 13:31:27 -0500

    Add files via upload

---
+++ Q5/PA1_Q5_Main.hpp
@@ -0,0 +1,18 @@
+//
+//  PA1_Q5_Main.hpp
+//  
+//
+//  Created by Nick Hardy on 10/10/21.
+//
+
+#ifndef PA1_Q5_Main_hpp
+#define PA1_Q5_Main_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+#endif /* PA1_Q5_Main_hpp */
