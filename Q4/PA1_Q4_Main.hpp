1a928c64ea4ca6ff18de1a8e025ea693f6ea367b
nckhrdy <91441308+nckhrdy@users.noreply.github.com>
2022-01-06 13:30:30 -0500

    Add files via upload

---
+++ Q4/PA1_Q4_Main.hpp
@@ -0,0 +1,18 @@
+//
+//  PA1_Q4_Main.hpp
+//  
+//
+//  Created by Nick Hardy on 10/10/21.
+//
+
+#ifndef PA1_Q4_Main_hpp
+#define PA1_Q4_Main_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+#endif /* PA1_Q4_Main_hpp */
+
